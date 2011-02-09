71ad6af32bf1928a29556dbe2fb7c222d9d9db84
TurboMonkeyNinja@gmail.com <TurboMonkeyNinja@gmail.com@a3ec9bf8-4a3e-46b8-35b9-ca89222f22af>
2011-02-09 08:37:20 +0000

    linked to me

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MyHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write(template.render('main.html', {}))
         self.response.out.write("""
-        <br><div style="font-size:medium">Alex Leutenegger 2011
+        <br><div style="font-size:medium"><a href="www.alexleut.com">Alex Leutenegger</a> 2011
         <a href="http://code.google.com/p/truth-table-gae/">Source</a></div>
         </body></html>""")
 
@@ -20,7 +20,7 @@ class MyHandler(webapp.RequestHandler):
         for line in truth_table(formula):
             self.response.out.write('<div>%s</div>' % line)
         self.response.out.write("""
-        <br><div style="font-size:medium">Alex Leutenegger 2011
+        <br><div style="font-size:medium"><a href="www.alexleut.com">Alex Leutenegger</a> 2011
         <a href="http://code.google.com/p/truth-table-gae/">Source</a></div>
         </body></html>""")
 
